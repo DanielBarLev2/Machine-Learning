1cb02a4419c70e69e4ad2813ed8d593d45fa4ec4
Daniel Bar Lev <Danielbl2010@gmail.com>
2023-05-17 11:36:18 +0300

    Add linear_forward and Activation Functions

--- A2/Neural_Network.py
+++ A2/Neural_Network.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def initialize_parameters(layer_dims: np.array, x,y) -> dict:
+# forward propagation
+def initialize_parameters(layer_dims: np.array) -> dict:
     """
     input: an array of the dimensions of each layer in the network.
     (layer 0 is the size of the flaened input, layer L is the output softmax)
@@ -13,9 +14,47 @@ def initialize_parameters(layer_dims: np.array, x,y) -> dict:
     l = len(layer_dims)
 
     for layer in range(1, len(layer_dims)):
-        parameters[f'W{layer}'] = np.randn(layer_dims[layer], layer_dims[layer - 1])
-        parameters[f'b{layer}'] = np.zeros(layer_dims[layer], 1)
+        parameters[f'W{layer}'] = np.random.randn(layer_dims[layer], layer_dims[layer - 1]) * 0.01
+        parameters[f'b{layer}'] = np.zeros((layer_dims[layer]), 1)
 
     return parameters
 
-# test:
+
+def linear_forward(A, W: np.array, b: np.array) -> tuple(int, dict):
+    """
+    Descripon: Implement the linear part of a layer's forward propagaon
+
+    input:
+    A – the activations of the previous layer
+    W – the weight matrix of the current layer (of shape [size of current layer, size of previous layer])
+    B – the bias vector of the current layer (of shape [size of current layer, 1])
+
+    Output:
+    Z – the linear component of the activations function (i.e., the value before applying the non-linear funcon)
+    linear_cache – a diconary containing A, W, b (stored for making the backpropagaon easier to compute)
+    """
+
+    Z = np.dot(W, A) + b
+    linear_cache = {A: A, W: W, b: b}
+
+    return Z, linear_cache
+
+
+def sigmoid(Z):
+    A = 1 / (1 + np.exp(-Z))
+    return A, Z
+
+
+def tanh(Z):
+    A = np.tanh(Z)
+    return A, Z
+
+
+def relu(Z):
+    A = np.maximum(0, Z)
+    return A, Z
+
+
+def leaky_relu(Z):
+    A = np.maximum(0.1 * Z, Z)
+    return A, Z
