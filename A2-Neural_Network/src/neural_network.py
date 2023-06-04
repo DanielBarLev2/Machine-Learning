1e1be82421ce661afb75b5b1b7c7397d2e6d74f5
Daniel Bar Lev <Danielbl2010@gmail.com>
2023-06-04 18:39:37 +0300

    add division to 5 folds

--- A2-Neural_Network/src/neural_network.py
+++ A2-Neural_Network/src/neural_network.py
@@ -1,32 +1,39 @@
-import numpy as np
-
 from application import l_layer_model, predict
+from sklearn.model_selection import KFold
 from keras.datasets import mnist
+import numpy as np
 
 # loads MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 # transform the data into a proper vector form and shape
-x_train = x_train.reshape(6000, -1).T
-y_train = y_train.reshape(-1, 6000)
+x_train = x_train.reshape(60000, -1).T
 x_test = x_test.reshape(10000, -1).T
-y_test = y_test.reshape(-1, 10000)
 
 # standardize the data
 x_train = x_train / 255
 x_test = x_test / 255
 
-print('Training set shape:', x_train.shape)
-print('Training labels shape:', y_train.shape)
-print('Test set shape:', x_test.shape)
-print('Test labels shape:', y_test.shape)
+# initialize the cross-validation object
+k_fold = KFold(n_splits=5, shuffle=True)
 
-# setting layers dims
-layers_dims = np.array([x_train.shape[0], 20, 7, 5, 10])
+# initialize parameters
+parameters = []
 
-parameters = l_layer_model(x_train, y_train, layers_dims, 0, learning_rate=0.009, num_iterations=100)
+# iterate over the folds and trains
+for train_index, val_index in k_fold.split(x_train):
+    # Get the training and validation data for this fold
+    x_fold_train, X_fold_test = x_train[train_index], x_test[val_index]
+    y_fold_train, y_fold_test = y_train[train_index], y_test[val_index]
 
-print("# pass 1")
+    # setting layers dims
+    layers_dims = np.array([x_fold_train.shape[0], 20, 7, 5, 10])
 
-# print the accuracy
-predict(x_test, parameters, y_test)
+    # train
+    parameters = l_layer_model(x_train=x_fold_train, y_train=y_fold_train, layer_dims=layers_dims, learning_rate=0.009,
+                               num_iterations=1)
+
+# predict using the parameters
+accuracy = predict(x_test=x_test, y_test=y_test, parameters=parameters)
+
+print(accuracy)
