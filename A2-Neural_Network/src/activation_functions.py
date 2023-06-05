fix softmax dx edit documentation

A2-Neural_Network/src/activation_functions.py
@@ -1,29 +1,15 @@
 import numpy as np
 
-def sigmoid(z: np.ndarray) -> tuple[np.array, np.ndarray]:
-    """
-    Input:
-    Z – the linear component of the activation function
-
-    Output:
-    activation – the activations of the layer
-    activations_cache – returns Z, which will be useful for the backpropagation
-    """
-
-    activation = 1 / (1 + np.exp(-z))
-    activations_cache = z
-
-    return activation, activations_cache
-
-
 def relu(z: np.array) -> tuple[np.array, np.ndarray]:
     """
+    Description: Implements forward propagation for a relu unit.
+
     Input:
-    Z – the linear component of the activation function
+    Z – the linear component of the activation function.
 
     Output:
-    activation – the activation of the layer
-    activation_cache – returns Z, which will be useful for the backpropagation
+    activation – the activation of the layer.
+    activation_cache – returns Z, which will be useful for the backpropagation.
     """
     activation = np.maximum(0, z)
     activations_cache = z
@@ -33,14 +19,14 @@ def relu(z: np.array) -> tuple[np.array, np.ndarray]:
 
 def relu_backward(da: np.ndarray, activation_cache: np.ndarray) -> np.array:
     """
-    Description: Implements backward propagation for a ReLU unit
+    Description: Implements backward propagation for a ReLU unit.
 
     Input:
-    dA – the post-activation gradient
-    activation_cache – contains z (stored during the forward propagation)
+    dA – the post-activation gradient.
+    activation_cache – contains z (stored during the forward propagation).
 
     Output:
-    dz – gradient of the cost with respect to Z
+    dz – gradient of the cost with respect to Z.
     """
 
     z = activation_cache
@@ -50,33 +36,16 @@ def relu_backward(da: np.ndarray, activation_cache: np.ndarray) -> np.array:
     return dz
 
 
-def sigmoid_backward(da: np.ndarray, activation_cache: np.ndarray) -> np.array:
-    """
-    Description: Implements backward propagation for a sigmoid unit
-
-    Input:
-    dA – the post-activation gradient
-    activation_cache – contains Z (stored during the forward propagation)
-
-    Output:
-    dZ – gradient of the cost with respect to Z
-    """
-
-    z = activation_cache
-    a, z = sigmoid(z=z)
-    dz = da * a * (1-a)
-
-    return dz
-
-
 def softmax(z: np.ndarray) -> tuple[np.array, np.ndarray]:
     """
+    Description: Implements forward propagation for a Softmax unit.
+
     Input:
-    Z – the linear component of the activation function
+    Z – the linear component of the activation function.
 
     Output:
-    activation – the activations of the layer
-    activations_cache – returns Z, which will be useful for the backpropagation
+    activation – the activations of the layer.
+    activations_cache – returns Z, which will be useful for the backpropagation.
     """
 
     activation = np.exp(z) / np.sum(np.exp(z), axis=0, keepdims=True)
@@ -87,7 +56,7 @@ def softmax(z: np.ndarray) -> tuple[np.array, np.ndarray]:
 
 def softmax_backward(da, activation_cache):
     """
-    Compute the backward propagation for the softmax activation function.
+    Description: Implements backward propagation for a Softmax unit.
 
     Arguments:
     da -- true labels, numpy array of shape (n_classes, m)
@@ -98,7 +67,7 @@ def softmax_backward(da, activation_cache):
     """
 
     z = activation_cache
-    a, z = sigmoid(z=z)
+    a, z = softmax(z=z)
     dz = da * a * (1-a)
 
     return dz