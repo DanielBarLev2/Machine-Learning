56cc6a4f2b658e2e7f91e3317f891e552d768b5c
Daniel Bar Lev <Danielbl2010@gmail.com>
2023-06-10 18:25:41 +0300

    Revert "edit doc update evaluate_model add prepares_folds"
    This reverts commit eabd8dea
    
    added batch division

--- A2-Neural_Network/src/model.py
+++ A2-Neural_Network/src/model.py
@@ -3,7 +3,7 @@ from backward import l_model_backward, update_parameters
 import numpy as np
 
 def l_layer_model(x_train: np.ndarray, y_train: np.ndarray, layer_dims: np.ndarray, learning_rate: float,
-                  num_iterations: int) -> dict:
+                  num_iterations: int, batch_size: int) -> dict:
     """
     Description: Implements L-layer neural network. All layers but the last should have the ReLU
     activation function, and the final layer will apply the sigmoid activation function.
@@ -31,17 +31,22 @@ def l_layer_model(x_train: np.ndarray, y_train: np.ndarray, layer_dims: np.ndarr
     # initialize cost list
     cost_list = []
 
+    x_train_batch, y_train_batch, num_batches  = create_batches(x_data=x_train, y_data=y_train, batch_size=batch_size)
+    batch_index = 0
+
     for i in range(num_iterations):
         # iterate over l layers to get the final last last_activation and the cache
-        last_activation, caches = l_model_forward(x_data=x_train, parameters=parameters)
+        last_activation, caches = l_model_forward(x_data=x_train_batch[batch_index], parameters=parameters)
 
-        cost = cross_entropy_loss(last_activation=last_activation, y_train=y_train)
+        cost = cross_entropy_loss(last_activation=last_activation, y_train=y_train_batch[batch_index])
 
         # iterate over L-layers backward to get gradients
-        gradients = l_model_backward(last_activation=last_activation, y_train=y_train, caches=caches)
+        gradients = l_model_backward(last_activation=last_activation, y_train=y_train_batch[batch_index], caches=caches)
 
         # update parameters
         parameters = update_parameters(parameters, gradients, learning_rate)
+
+        batch_index += 1 
 
         print(i)
         if i % 10 == 0 and i != 0:
@@ -51,13 +56,36 @@ def l_layer_model(x_train: np.ndarray, y_train: np.ndarray, layer_dims: np.ndarr
     return parameters
 
 
+def create_batches(x_data: np.ndarray, y_data: np.ndarray, batch_size: int) -> tuple[list, list, int]:
+    num_samples = x_data.shape[1]
+    num_batches = num_samples // batch_size
+    batches_x = []
+    batches_y = []
+
+    # Create full-sized batches
+    for i in range(num_batches):
+        batch_x = x_data[:, i * batch_size: (i + 1) * batch_size]
+        batch_y = y_data[:, i * batch_size: (i + 1) * batch_size]
+        batches_x.append(batch_x)
+        batches_y.append(batch_y)
+
+    # Create the last smaller batch, if necessary
+    if num_samples % batch_size != 0:
+        batch_x = x_data[:, num_batches * batch_size:]
+        batch_y = y_data[:, num_batches * batch_size:]
+        batches_x.append(batch_x)
+        batches_y.append(batch_y)
+
+    return batches_x, batches_y, num_batches
+
+
 def predict(x_test: np.ndarray, y_test: np.ndarray, parameters: dict) -> str:
     """
     Description: The function receives an input data and the true labels and calculates the accuracy of
     the trained neural network on the data.
 
     Input:
-    X – the input data, a numpy array of shape (height*width, number_of_examples)
+    x_data – the input data, a numpy array of shape (height*width, number_of_examples)
     Y – the “real” labels of the data, a vector of shape (num_of_classes, number of
     examples)
     Parameters – a python dictionary containing the DNN architecture’s parameters
